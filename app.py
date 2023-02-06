cade40271e18c304199726b3c2bb9ad3a9756974
John Johnson <caellwyn@gmail.com>
2023-02-05 16:15:52 -0800

    add lime explainer

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import numpy as np
+from lime import lime_image
+from skimage.segmentation import mark_boundaries
+import matplotlib.pyplot as plt
+from PIL import Image
 from src import *
 
 #Create the model
@@ -19,3 +24,17 @@ if image:
     response = model.predict_pet(image)
     result.write(response)
 
+if image:
+    st.header('I think this because I see...\nGreen gives me confidence, red gives me doubt')
+    conv_image = model.convert_image(image)
+    explainer = lime_image.LimeImageExplainer()
+    exp = explainer.explain_instance(conv_image[0],
+                                model.model.predict,)
+    
+    image, mask = exp.get_image_and_mask(0,
+                                     positive_only=False, 
+                                     negative_only=False,
+                                     hide_rest=False,
+                                     min_weight=.05
+                                    )
+    st.image(mark_boundaries(image, mask), use_column_width='always')
